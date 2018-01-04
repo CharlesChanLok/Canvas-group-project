94114e4e7bb210116edb557c18501791d809674d
CharlesChanLok <charleschanlok@gmail.com>
2018-01-04 21:56:59 +0800

    added another color selector from jscolor library

--- js/color-selector.js
+++ js/color-selector.js
@@ -1,5 +1,12 @@
+/*
 function selectColor(event) {
     contextDraft.fillStyle = event.target.value;
     contextReal.fillStyle = event.target.value;
     contextReal.strokeStyle = event.target.value;
+} */
+
+function update(jscolor) {
+    contextDraft.fillStyle = '#' + jscolor;
+    contextReal.fillStyle = '#' + jscolor;
+    contextReal.strokeStyle = '#' + jscolor;
 }
