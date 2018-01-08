2f3af7d025dac37ceea9bab2cda7c9dfc992ece1
CharlesChanLok <charleschanlok@gmail.com>
2018-01-08 23:20:13 +0800

    improved slider

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,4 @@
+/* Line width */
 var lineWidth= document.getElementById("line-width");
 var outputLine = document.getElementById("lineWidth");
 outputLine.innerHTML = lineWidth.value;
@@ -5,11 +6,41 @@ outputLine.innerHTML = lineWidth.value;
 lineWidth.oninput = function() {
   outputLine.innerHTML = this.value;
 }
+/* plus  and minus section of Line width*/
+document.getElementsByClassName("fa-plus")[0].addEventListener('click', () => {
+  this.lineWidth.value++;
+  outputLine.innerHTML = this.lineWidth.value;
+  contextReal.lineWidth++;
+    contextDraft.lineWidth++ ;
+});
 
+document.getElementsByClassName("fa-minus")[0].addEventListener('click', () => {
+  this.lineWidth.value--;
+  outputLine.innerHTML = this.lineWidth.value;
+  contextReal.lineWidth--;
+  contextDraft.lineWidth--;
+
+});
+/* Polygon side*/
 var sideValue = document.getElementById("sides");
 var outputSide = document.getElementById("sideValue");
-outputSide.innerHTML = sideValue .value;
+outputSide.innerHTML = sideValue.value;
 
-lineValue.oninput = function() {
+sideValue.oninput = function() {
   outputSide.innerHTML = this.value;
-}
+};
+
+/* plus  and minus section of Polygon*/
+document.getElementsByClassName("fa-plus")[1].addEventListener('click', () => {
+  this.sideValue.value++;
+  outputSide.innerHTML = this.sideValue.value;
+  sides++;
+});
+
+document.getElementsByClassName("fa-minus")[1].addEventListener('click', () => {
+  this.sideValue.value--;
+  outputSide.innerHTML = this.sideValue.value;
+  sides--;
+
+});
+
