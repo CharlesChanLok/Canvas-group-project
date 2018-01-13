52a9399529d23f9e0df1cc1e097ddebd5f7502e4
CharlesChanLok <charleschanlok@gmail.com>
2018-01-13 22:36:43 +0800

    changed to original image width and height in upload-image

--- js/upload-image.js
+++ js/upload-image.js
@@ -5,7 +5,7 @@ function uploadImage(ctx, canvas, event) {
         let img = new Image();
         img.src = event.target.result;
         img.onload = function () {
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            ctx.drawImage(img, 0, 0, img.width, img.height);
         }
     }
 }
